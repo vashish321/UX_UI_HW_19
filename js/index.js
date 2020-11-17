954eea25bb76ed062a415f4c0e5f34684554af1c
vashish321 <63172589+vashish321@users.noreply.github.com>
2020-11-17 10:37:23 -0400

    new

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+document.getElementById("myBtn").addEventListener("click", function(){
+    alert("first Button");
+    // $(".fadeMe").fadeToggle("slow");
+});
+document.getElementById("myBtn2").addEventListener("click", function(){
+    alert("Second Button");
+    // $(".fadeMe").fadeToggle("slow");
+});
+document.getElementById("myBtn3").addEventListener("click", function(){
+    alert("third Button");
+    // $(".fadeMe").fadeToggle("slow");
+});
+document.getElementById("myBtn4").addEventListener("click", function(){
+    alert("fourth Button");
+    // $(".fadeMe").fadeToggle("slow");
+});
